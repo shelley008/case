119f12e0fde0254b3e795c3e92a8fd07c375dc61
shelley008 <zhanggh@hexmeet.com>
2018-10-08 18:01:22 +0800

    de

--- src/store/storeTypes.js
+++
@@ -1,6 +0,0 @@
-export const ADD_FLOW = 'ADD_FLOW'
-export const SET_CHARGE_DATAS = 'SET_CHARGE_DATAS'
-
-export const ADD_NOTES = 'ADD_NOTES'
-export const DELETE_NOTES = 'DELETE_NOTES'
-export const GET_NOTES = 'GET_NOTES'
